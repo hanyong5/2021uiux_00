b5d73fff1563cc20750c7c2653e50ac0ab37d0b7
hanyogn5 <hanyong5@naver.com>
2022-03-11 09:44:45 +0900

    gallery v1

--- 00javascript/20220310_gallery/js/script.js
+++ 00javascript/20220310_gallery/js/script.js
@@ -1,1 +1,47 @@
 console.log("연결완료");
+
+$(document).ready(function(){
+
+    // tab 이미지
+    $(".thumb li").click(function(){
+        let i = $(this).index();
+        console.log(i);
+        $("#image p").hide().eq(i).show();
+    })
+
+
+    let current = 0;
+ 
+ 
+    // $(".right").click(function(){
+    //     current++; //current = current + 1;
+    // });
+    // $(".left").click(function(){
+    //     current--;
+    // })
+
+
+    //버튼적용
+    $(".right").click(function(){ // next
+        current++; //current = current + 1;
+        if(current == 3) current = 0;
+        console.log(current);
+        move();
+    });
+    $(".left").click(function(){  // prev
+        current--;
+        if(current == -1) current = 2;
+        console.log(current);
+        move();
+    });
+
+    function move(){
+        //$(".thumb ul").css("left",current * -760);
+        
+        $(".thumb ul").css({left : current * -100 + "%" });
+        
+        // let cNum = current * -760;
+        // $(".thumb ul").stop().animate({left:cNum},300)
+        
+    }
+})
